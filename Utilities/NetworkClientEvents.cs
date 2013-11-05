2cbc295a61ff3048551fbbf90db36d9abc711066
Hani Amar <HaniAmar@users.noreply.github.com>
2013-11-06 01:43:12 +0300

    Initial commit

---
+++ Utilities/NetworkClientEvents.cs
@@ -0,0 +1,32 @@
+﻿namespace Hani.Utilities
+{
+    internal abstract partial class NetworkClient
+    {
+        internal delegate void StateHandler();
+        internal event StateHandler OnConnecting, OnConnected, OnDisconnected, OnFailedToConnect;
+
+        protected void Connecting()
+        {
+            if (!DisplayEvents) return;
+            if (OnConnecting != null) new StateHandler(OnConnecting)();
+        }
+
+        protected void Connected()
+        {
+            if (!DisplayEvents) return;
+            if (OnConnected != null) new StateHandler(OnConnected)();
+        }
+
+        protected void Disconnected()
+        {
+            if (!DisplayEvents) return;
+            if (OnDisconnected != null) new StateHandler(OnDisconnected)();
+        }
+
+        protected void FailedToConnect()
+        {
+            if (!DisplayEvents) return;
+            if (OnFailedToConnect != null) new StateHandler(OnFailedToConnect)();
+        }
+    }
+}
---
+++ Utilities/ProfileOptimizer.cs
@@ -0,0 +1,29 @@
+﻿using System.Runtime;
+
+namespace Hani.Utilities
+{
+    internal static class ProfileOptimizer
+    {
+        internal static string FileRoamingPath { get; private set; }
+        internal static string FileAppPath { get; private set; }
+
+        private const string fileName = @"\profile";
+
+        static ProfileOptimizer()
+        {
+            _set();
+        }
+
+        private static void _set()
+        {
+            FileRoamingPath = AppSettings.RoamingPath + fileName;
+            FileAppPath = DirectoryHelper.CurrentDirectory + fileName;
+        }
+
+        internal static void Load()
+        {
+            ProfileOptimization.SetProfileRoot(AppSettings.SameAppPath ? DirectoryHelper.CurrentDirectory : AppSettings.RoamingPath);
+            ProfileOptimization.StartProfile(fileName);
+        }
+    }
+}
